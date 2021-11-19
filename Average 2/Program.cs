da3c553d163f3b3681e2a06d06acb16c903007b1
Pedro Henrique Teixeira de Souza <phtsouza@gmail.com>
2021-11-19 14:09:13 -0300

    Add 2 questions

---
+++ Average 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Average_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, AVERAGE;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            A *= 2.0;
+            B *= 3.0;
+            C *= 5.0;
+
+            AVERAGE = (A + B + C) / 10;
+
+            Console.WriteLine("MEDIA = " + AVERAGE.ToString("F1", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Difference/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D;
+            int DIFERENCA;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            DIFERENCA = (A * B) - (C * D);
+
+            Console.WriteLine("DIFERENCA = " + DIFERENCA);
+            Console.ReadLine();
+        }
+    }
+}
