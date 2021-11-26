4ddf74eaa96e6c291869493159fc7d5f0e177626
Pedro Henrique Teixeira de Souza <phtsouza@gmail.com>
2021-11-25 22:24:52 -0300

    add 1 question

---
+++ Subsequences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Subsequences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qntdCasos;
+            int qntdString;
+            string principal, subString;
+            int i, j;
+
+            qntdCasos = int.Parse(Console.ReadLine());
+
+            for(i = 0; i < qntdCasos; i++)
+            {
+                principal = Console.ReadLine();
+                qntdString = int.Parse(Console.ReadLine());
+
+                for(j = 0; j< qntdString; j++)
+                {
+                    subString = Console.ReadLine();
+
+                    if (principal.Contains(subString))
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No");
+                    }
+                }    
+            }
+        }
+    }
+}
